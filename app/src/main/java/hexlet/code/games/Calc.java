5afef23e9359d31ecf5e9876a8ae6dfe32052d2d
Lee Xeniya <leexenya@gmail.com>
2025-05-13 14:28:19 +0300

    added limit

--- app/src/main/java/hexlet/code/games/Calc.java
+++ app/src/main/java/hexlet/code/games/Calc.java
@@ -9,12 +9,13 @@ public class Calc {
         var ROUNDS = 3;
         Random random = new Random();
         var dataGame = new String[ROUNDS][];
-        final int limit = 10;
+        final int limit1 = 10;
+        final int limit2 = 3;
         for (var i = 0; i < ROUNDS; i++) {
-            int number1 = random.nextInt(1, limit);
-            int number2 = random.nextInt(1, limit);
+            int number1 = random.nextInt(1, limit1);
+            int number2 = random.nextInt(1, limit1);
             String[] signs = {"-", "+", "*"};
-            int sign = random.nextInt(0, limit);
+            int sign = random.nextInt(0, limit2);
             var correctAnswer = calculate(number1, number2, signs[sign]);
 
             dataGame[i] = new String[] {number1 + " " + signs[sign] + " "
